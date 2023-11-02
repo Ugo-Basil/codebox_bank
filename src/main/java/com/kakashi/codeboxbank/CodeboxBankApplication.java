cb1ffcb648fb26816fc4a90f510aefd5d55ce6f6
Basil <ohuruogubasil@gmail.com>
2023-11-02 11:30:22 +0100

    added:entities

---
+++ src/main/java/com/kakashi/codeboxbank/CodeboxBankApplication.java
@@ -0,0 +1,13 @@
+package com.kakashi.codeboxbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeboxBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeboxBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakashi/codeboxbank/CodeboxBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakashi.codeboxbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeboxBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
